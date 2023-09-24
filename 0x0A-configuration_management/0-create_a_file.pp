afd91413eb62777e07fdce4232d852a83fc8b25b
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-09-24 15:11:22 +0100

    fix: fix puppet-lint on create_a_file.pp file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp/school':
-  mode => '0744',
-  ensure => present,
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
