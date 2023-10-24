6a3e9f04ccb4d62127abcbf78fef285143a85e28
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-24 08:48:06 +0100

    feat:  API  mandatory tasks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+"""Returns todo list information for a given ID."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    user_name = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user_id = sys.argv[1]
+
+    with open(f'{user_id}.json', 'w') as jsonFile:
+        data = {user_id: []}
+        for todo in todos:
+            data[user_id].append(
+                {'task': todo['title'],
+                 'completed': todo['completed'],
+                 'username': user_name})
+        json.dump(data, jsonFile)
