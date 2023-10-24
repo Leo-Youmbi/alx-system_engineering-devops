6478cf60f5ccf419d146a22510a5ae6ddaad662f
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-24 09:39:12 +0100

    feat:  API  mandatory tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+"""Returns todo list information for a given ID."""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    todos = requests.get(url + "todos").json()
+
+    with open('todo_all_employees.json', 'w') as jsonFile:
+        data = {}
+        for user in users:
+            data[user['id']] = [{
+                            'username': user["username"],
+                            'completed': todo['completed'],
+                            'task': todo['title']
+                        } for todo in todos if todo['userId'] == user['id']]
+        json.dump(data, jsonFile)
