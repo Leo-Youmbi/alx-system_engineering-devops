acefd158e8f61080e1437105354891669cb83529
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-11-10 05:44:34 +0100

    feat: Web stack debugging #3  task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixing error 500 in apache server with puppet
+
+exec { 'fix the typing error':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
